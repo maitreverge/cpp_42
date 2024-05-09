44b57f9fe1d501ef4e3ccce18ff00cba63199412
“florianverge” <verge.f@gmail.com>
2024-05-09 13:30:30 +0200

    add Doxygen doc

--- cpp_00/ex00/src/megaphone.cpp
+++ cpp_00/ex00/src/megaphone.cpp
@@ -6,12 +6,21 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/09 11:55:34 by flverge           #+#    #+#             */
-/*   Updated: 2024/05/09 13:03:49 by flverge          ###   ########.fr       */
+/*   Updated: 2024/05/09 13:29:56 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
+/**
+ * @brief Main function for megaphone.cpp
+ * If ac == 1 => Outputs "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+ * Otherwise, converts the given parametters in uppercase
+ * 
+ * @param ac 
+ * @param av 
+ * @return int 
+ */
 int main(int ac, char **av)
 {
 	if (ac == 1)
@@ -22,9 +31,9 @@ int main(int ac, char **av)
 		{
 			for (int j = 0; av[i][j] != 0; j++)
 			{
-				// Convert to upper each letter before printing it out.
+				// Convert to upper each letter before printing it out
+				// ! Need to cast it in a char, otherwise it prints out actual ASCII values
 				std::cout << (char)std::toupper(av[i][j]);
-				// Need to cast it in a char, otherwise it prints out actual ASCII values.
 			}
 			// One space between words 
 			std::cout << " ";
@@ -32,6 +41,6 @@ int main(int ac, char **av)
 		// Then back at the line
 		std::cout << std::endl;
 	}
-	// must return a value in c++ main
+	// Must return a value in C++ main function
 	return 0;
 }
