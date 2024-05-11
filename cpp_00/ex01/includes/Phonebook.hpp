fb77983efecb633b45c4067b6adf910b502bc8d7
“maitreverge_42” <verge.f@gmail.com>
2024-05-11 10:36:43 +0200

    init phonebook exercices

---
+++ cpp_00/ex01/includes/Phonebook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 10:28:06 by flverge           #+#    #+#             */
+/*   Updated: 2024/05/11 10:36:31 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class PhoneBook
+{
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+	
+		//char
+		
+};
+
+class Contact
+{
+	public:
+		
+};
---
+++ cpp_00/ex01/src/phonebook.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   phonebook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/11 10:29:47 by flverge           #+#    #+#             */
+/*   Updated: 2024/05/11 10:32:55 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Phonebook.hpp"
+
+/**
+ * @brief Construct a new Phone Book:: Phone Book object
+ * 
+ */
+PhoneBook::PhoneBook(void)
+{
+	return ;
+}
+
+/**
+ * @brief Destroy the Phone Book:: Phone Book object
+ * 
+ */
+PhoneBook::~PhoneBook(void)
+{
+	return;
+}
+
