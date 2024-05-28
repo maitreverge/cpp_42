9a28a7e267ddf0aa764ea6b608d25e36e81e7ad2
“florianverge” <verge.f@gmail.com>
2024-05-28 20:29:27 +0200

    init weapon class

--- cpp_01/ex03/includes/Weapon.hpp
+++ cpp_01/ex03/includes/Weapon.hpp
@@ -6,7 +6,36 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 09:57:51 by flverge           #+#    #+#             */
-/*   Updated: 2024/05/28 09:57:52 by flverge          ###   ########.fr       */
+/*   Updated: 2024/05/28 20:28:47 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+#include <string>
+#include "HumanA.hpp"
+#include "HumanB.hpp"
+
+// ANSII colors codes
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define WHITE   "\033[37m"
+#define BOLD    "\033[1m"
+
+class Weapon
+{
+	private:
+		std::string _type;
+	public:
+		Weapon( void );
+		~Weapon( void );
+		const std::string&	getType( void )const;
+		void			setType( std::string input );
+};
--- cpp_01/ex03/src/Weapon.cpp
+++ cpp_01/ex03/src/Weapon.cpp
@@ -6,7 +6,20 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 09:57:45 by flverge           #+#    #+#             */
-/*   Updated: 2024/05/28 09:57:46 by flverge          ###   ########.fr       */
+/*   Updated: 2024/05/28 20:28:22 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+
+Weapon::Weapon( void ){}
+
+Weapon::~Weapon( void ){}
+
+const std::string& Weapon::getType( void )const{
+	return this->_type;
+}
+
+void Weapon::setType( std::string input){
+	this->_type = input;
+}
