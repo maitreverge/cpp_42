af2607ad1ddc8728e04d003baa4cf4c3fc792cd7
“florianverge” <verge.f@gmail.com>
2024-05-28 21:00:15 +0200

    refactor: Update HumanA::attack() to print weapon type

--- cpp_01/ex03/src/HumanA.cpp
+++ cpp_01/ex03/src/HumanA.cpp
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 09:57:49 by flverge           #+#    #+#             */
-/*   Updated: 2024/05/28 20:42:29 by flverge          ###   ########.fr       */
+/*   Updated: 2024/05/28 20:45:54 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@ HumanA::~HumanA()
 }
 
 void HumanA::attack( void ){
-	printColor(RED, this->getName());
+	printColorNoEndl(RED, this->getName());
+	printNoEndl(" attacks with their ");
+	printColor(GREEN, _weapon.getType());
 }
 
 const std::string& HumanA::getName( void )const{
