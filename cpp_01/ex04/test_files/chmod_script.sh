1dc4a5f108d3722a1799f18ebfd6d81a49bb7e27
“florianverge” <verge.f@gmail.com>
2024-06-02 15:35:49 +0200

    Shell scripts for chmod automatisation

---
+++ cpp_01/ex04/test_files/chmod_script.sh
@@ -0,0 +1,26 @@
+# /bin/sh
+
+echo ""
+echo "\e[1;31mBefore script\e[0m"
+echo "----------"
+echo ""
+ls -la
+
+# reset all authorizations
+chmod 000 *.txt
+
+# READ + WRITE to koala
+chmod +rw koala.txt
+
+# Only READ rights
+chmod +r NO_WRITE_RIGHTS.txt
+
+# Only WRITE
+chmod +w NO_READ_RIGHTS.txt
+
+
+echo ""
+echo "\e[1;32mAfter script\e[0m"
+echo "----------"
+echo ""
+ls -la
