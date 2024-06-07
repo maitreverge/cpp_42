209cd06223598d0d8059fd4af08ffa5ef3598d55
“maitreverge_42” <verge.f@gmail.com>
2024-06-07 14:52:56 +0200

    Adding fixedPoint maths functions

--- cpp_02/ex01/includes/Fixed.hpp
+++ cpp_02/ex01/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:48:18 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/06 16:33:56 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/07 14:51:32 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,26 @@ class Fixed
 {
 private:
 
+	int _fixedPointValue;
+	static const int _fractionalBits = 8; 
 
 public:
 
+	Fixed ( void );
+	
+	// Constructors
 	Fixed( const int intInput );
 	Fixed( const float floatInput );
-	Fixed( const Fixed& copy );
-	Fixed& operator=( const Fixed& right_operator);
+	Fixed( const Fixed& copy ); // Copy constructor
+	
+	// Destructors
 	~Fixed();
+	
+	// Class assignment operator overload
+	Fixed& operator=( const Fixed& right_operator);
+	
+	// Getter fromthe subject
+	float	toFloat( void ) const;
+	int		toInt( void ) const;
 
 };
--- cpp_02/ex01/src/Fixed.cpp
+++ cpp_02/ex01/src/Fixed.cpp
@@ -6,25 +6,50 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:48:22 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/06 16:32:04 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/07 14:49:43 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
+#include "../includes/Fixed.hpp"
 
 Fixed::Fixed( void ){}
+
+Fixed::Fixed( const int intInput ){
+	
+	// Multiply input by 2^fractionalBits, so 256.
+	this->_fixedPointValue = intInput * pow(2, this->_fractionalBits);
+
+	printColor(BOLD_HIGH_INTENSITY_GREEN, "✅ Int Constructor Called ✅");
+	
+}
+
+Fixed::Fixed( const float floatInput ){
+	
+	this->_fixedPointValue = static_cast<int>(roundf(floatInput * pow(2, this->_fractionalBits)));
+	printColor(BOLD_GREEN, "✅ Float Constructor Called ✅");
+}
 
 
 Fixed::Fixed( const Fixed& copy ){ *this = copy; }
 
-
-Fixed::~Fixed( void ){}
+// Destructor
+Fixed::~Fixed( void ){
+	
+	printColor(BOLD_RED, "❌ Destructor Called ❌");
+}
 
 
 Fixed& Fixed::operator=( const Fixed& right_operator ){
 	if (this != &right_operator){
-		// Reassign every value with the getter value 
-		// this->_foo = right_operator.getFoo()
+		this->_fixedPointValue = right_operator._fixedPointValue;
 	}
 	return *this;
+}
+
+float Fixed::toFloat( void )const{
+	return static_cast<float>( this->_fixedPointValue / pow(2, this->_fractionalBits) );
+}
+
+int Fixed::toInt( void )const{
+	return ( this->_fixedPointValue / pow(2, this->_fractionalBits) );
 }
