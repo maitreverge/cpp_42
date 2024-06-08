d0a36a9440cdbe5decb4f3b0bc423e19adcc7f3e
“florianverge” <verge.f@gmail.com>
2024-06-08 16:41:07 +0200

    work on cpp_02_ex_02

--- cpp_02/ex02/includes/Fixed.hpp
+++ cpp_02/ex02/includes/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:48:18 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/08 08:58:11 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/08 16:40:47 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,42 @@ private:
 
 public:
 
-	Fixed( void );
-	Fixed( const Fixed& copy );
-	Fixed& operator=( const Fixed& right_operator);
-	~Fixed();
+	Fixed( void ); // done
+	Fixed( const int intInput ); // done
+	Fixed( const float floatInput ); // done
+	Fixed( const Fixed& copy ); // done
+	Fixed& operator=( const Fixed& right_operator ); // done
+	~Fixed(); // done
 
-	const int& getFixedPointValue( void )const;
+	// Operateurs de comparaison
+	bool operator<( const Fixed& right_operator ); // done
+	bool operator<=( const Fixed& right_operator ); // done
+	bool operator>( const Fixed& right_operator ); // done
+	bool operator>=( const Fixed& right_operator ); // done
+	bool operator==( const Fixed& right_operator ); // done
+	bool operator!=( const Fixed& right_operator ); // done
+
+	// Operateurs arithmetiques
+	Fixed operator+( const Fixed& right_operator );
+	Fixed operator-( const Fixed& right_operator );
+	Fixed operator*( const Fixed& right_operator );
+	Fixed operator/( const Fixed& right_operator ); // ! Div by zero edge case
+	
+	// Operateurs d'incrementation
+	Fixed operator++( void );
+	Fixed operator++( int ) ;
+	Fixed operator--( void );
+	Fixed operator--( int );
+
+	static Fixed& min( Fixed& nb1, Fixed& nb2);
+	static const Fixed& min( const Fixed& nb1, const Fixed& nb2);
+	static Fixed& max( Fixed& nb1, Fixed& nb2);
+	static const Fixed& max( const Fixed& nb1, const Fixed& nb2);
+
+	// Others member functions
+	const int&	getFixedPointValue( void )const;
+	float		toFloat( void )const;
+	int			toInt( void )const;
 
 };
 
--- cpp_02/ex02/src/Fixed.cpp
+++ cpp_02/ex02/src/Fixed.cpp
@@ -6,13 +6,24 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:48:22 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/08 08:58:45 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/08 16:40:40 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
 Fixed::Fixed( void ){}
+
+Fixed::Fixed( const int intInput ){
+	
+	// Multiply input by 2^fractionalBits, so 256.
+	this->_fixedPointValue = intInput * pow(2, this->_fractionalBits);
+}
+
+Fixed::Fixed( const float floatInput ){
+	
+	this->_fixedPointValue = static_cast<int>(roundf(floatInput * pow(2, this->_fractionalBits)));
+}
 
 
 Fixed::Fixed( const Fixed& copy ){ *this = copy; }
@@ -23,17 +34,76 @@ Fixed::~Fixed( void ){}
 
 Fixed& Fixed::operator=( const Fixed& right_operator ){
 	if (this != &right_operator){
-		// Reassign every value with the getter value 
-		// this->_foo = right_operator.getFoo()
+		this->_fixedPointValue = right_operator._fixedPointValue;
 	}
 	return *this;
 }
 
-const int& Fixed::getFixedPointValue( void )const{
-	return this->_fixedPointValue;
+// Operateurs de comparaison
+
+bool Fixed::operator<( const Fixed& right_operator ){
+	
+	return (this->_fixedPointValue < right_operator._fixedPointValue)
+}
+
+bool Fixed::operator<=( const Fixed& right_operator ){
+	
+	return (this->_fixedPointValue <= right_operator._fixedPointValue)
+}
+
+bool Fixed::operator>( const Fixed& right_operator ){
+	
+	return (this->_fixedPointValue > right_operator._fixedPointValue)
+}
+
+bool Fixed::operator>=( const Fixed& right_operator ){
+	
+	return (this->_fixedPointValue >= right_operator._fixedPointValue)
+}
+
+bool Fixed::operator==( const Fixed& right_operator ){
+	
+	return (this->_fixedPointValue == right_operator._fixedPointValue)
+}
+
+bool Fixed::operator!=( const Fixed& right_operator ){
+	
+	return (this->_fixedPointValue != right_operator._fixedPointValue)
+}
+
+// Operateurs arithmetiques
+
+Fixed Fixed::operator+( const Fixed& right_operator ){
+	
+	return Fixed(this->_fixedPointValue + right_operator._fixedPointValue);
+}
+
+Fixed Fixed::operator-( const Fixed& right_operator ){
+	
+}
+
+Fixed Fixed::operator*( const Fixed& right_operator ){
+	
+}
+
+Fixed Fixed::operator/( const Fixed& right_operator ){
+	
 }
 
 
+// Others member functions
+const int& Fixed::getFixedPointValue( void )const{
+	return this->_fixedPointValue;
+}
+float Fixed::toFloat( void )const{
+
+	return static_cast<float>( this->_fixedPointValue / pow(2, this->_fractionalBits) );
+}
+
+int Fixed::toInt( void )const{
+	
+	return ( this->_fixedPointValue / pow(2, this->_fractionalBits) );
+}
 
 ostream& operator<<( ostream& output_stream, const Fixed& right_input ){
 	//output_stream << right_input.PutAGetterFunctionhere();
