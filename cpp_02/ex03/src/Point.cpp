08150cf7de4f95833fc53bb3103b9a1910b83914
“florianverge” <verge.f@gmail.com>
2024-06-10 22:54:58 +0200

    Can't opeator= with const values

--- cpp_02/ex03/src/Point.cpp
+++ cpp_02/ex03/src/Point.cpp
@@ -6,17 +6,16 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:51:20 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/10 20:25:45 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/10 22:54:14 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Point.hpp"
-#include "../includes/Fixed.hpp"
 
 Point::Point( void ) :
 	x(Fixed(0)), y(Fixed(0)){}
 
-Point::Point( const float inputX, const float inputY):
+Point::Point( const float inputX, const float inputY ):
 	x(Fixed(inputX)), y(Fixed(inputY)){}
 
 Point::Point( const Point& copy ){ *this = copy; }
@@ -26,11 +25,12 @@ Point::~Point( void ){}
 
 
 Point& Point::operator=( const Point& right_operator ){
-	if (this != &right_operator){
-		this->x = right_operator.x;
-		this->y = right_operator.y;
-	}
-	return *this;
+	// if (this != &right_operator){
+	// 	this->x = right_operator.x;
+	// 	this->y = right_operator.y;
+	// }
+	// return *this;
+	return Point((right_operator.x.getFixedPointValue()), (right_operator.y.getFixedPointValue()));
 }
 
 
--- cpp_02/ex03/src/bsp.cpp
+++ cpp_02/ex03/src/bsp.cpp
@@ -6,9 +6,30 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/05 10:51:36 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/10 20:14:01 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/10 22:22:13 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Point.hpp"
 
+/*
+
+def is_point_in_triangle(px, py, ax, ay, bx, by, cx, cy):
+    v0 = (cx - ax, cy - ay)
+    v1 = (bx - ax, by - ay)
+    v2 = (px - ax, py - ay)
+
+    dot00 = v0[0] * v0[0] + v0[1] * v0[1]
+    dot01 = v0[0] * v1[0] + v0[1] * v1[1]
+    dot02 = v0[0] * v2[0] + v0[1] * v2[1]
+    dot11 = v1[0] * v1[0] + v1[1] * v1[1]
+    dot12 = v1[0] * v2[0] + v1[1] * v2[1]
+
+    denom = dot00 * dot11 - dot01 * dot01
+    u = (dot11 * dot02 - dot01 * dot12) / denom
+    v = (dot00 * dot12 - dot01 * dot02) / denom
+
+    return u >= 0 and v >= 0 and (u + v) <= 1
+
+
+*/
