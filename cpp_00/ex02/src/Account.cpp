53f860b1060f5862cfb99887495c79c9b37f944f
“florianverge” <verge.f@gmail.com>
2024-06-11 18:46:22 +0200

    bank exercice compiling (finally)

--- cpp_00/ex02/src/Account.cpp
+++ cpp_00/ex02/src/Account.cpp
@@ -6,17 +6,24 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 17:53:54 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/11 18:21:44 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/11 18:45:30 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Account.hpp"
+#include <iostream>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
 
 // Constructors and Destructors
 Account::Account( void ){}
 
-Account::Account( int initial_desposit){
+Account::Account( int initial_desposit ){
 	
+	std::cout << "Hello" << std::endl;
 }
 
 Account::~Account(){}
@@ -32,11 +39,11 @@ int Account::getTotalAmount( void ){
 }
 
 int Account::getNbDeposits( void ){
-	return _totalNbDeposits;
+	return Account::_totalNbDeposits;
 }
 
 int Account::getNbWithdrawals( void ){
-	return _totalNbWithdrawals;
+	return Account::_totalNbWithdrawals;
 }
 
 void Account:: displayAccountsInfos( void ){
--- cpp_00/ex02/src/main.cpp
+++ cpp_00/ex02/src/main.cpp
@@ -10,7 +10,7 @@
 #include <vector>
 #include <algorithm>
 #include <functional>
-#include "../includes/"
+#include "../includes/Account.hpp"
 
 
 int		main( void ) {
