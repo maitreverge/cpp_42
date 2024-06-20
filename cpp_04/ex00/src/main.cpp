fee9e10f76faac8267fc9e02f40c1d918915bed4
“maitreverge_42” <verge.f@gmail.com>
2024-06-20 13:29:37 +0200

    refined test for cpp-04_ex_00

--- cpp_04/ex00/src/main.cpp
+++ cpp_04/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/18 21:36:50 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/20 13:17:14 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/20 13:27:28 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,65 +20,55 @@
 
 int main( void ){
 
-	// We'll declare a pointer to a new Animal.
-	Animal *a1 = new Animal();
-
+	extraLine();
+	print("--------------------------------------------------------------");
+	extraLine();
 	
-	// No surprise, a1 is an Animal, then it will make a sound from Aminal class.
-	a1->makeSound();
+	// We'll declare a pointer to a new Animal.
+	{	
+		Animal *a1 = new Animal();
+	
+		
+		// No surprise, a1 is an Animal, then it will make a sound from Aminal class.
+		a1->makeSound();
+		delete a1;
+		
+	}
 
+	print("--------------------------------------------------------------");
 	extraLine();
 	
 	// Let's declare now 2 Animal pointers which contains a cat, and another one for a dog.
+	{
+			
+		Animal *a2 = new Cat("Croquette");
+		
+		Animal *a3 = new Dog("Hercule");
+		
+		// makeSound is both a method from Animal AND Cat.
+		a2->makeSound();
+		
+		// makeSound is both a method from Animal AND Dog.
+		a3->makeSound();
 
-	Animal *a2 = new Cat("Croquette");
-	
-	Animal *a3 = new Dog("Hercule");
-	
-	// makeSound is both a method from Animal AND Cat.
-	a2->makeSound();
-	
-	// makeSound is both a method from Animal AND Dog.
-	a3->makeSound();
 
+		delete a2;
+		delete a3;
+		
+	}
 	print("--------------------------------------------------------------");
+	extraLine();
 
 	// Now let's make test from A WrongAnimal and WrongCat
+	{
 
-	WrongAnimal *a4 = new WrongCat("Bad Cat");
+		WrongAnimal *a4 = new WrongCat("Bad Cat");
 
-	// Because base class has not been declared as virtual, despite a4 being a Cat, a4 will still displays WrongAnimal sound
-	a4->makeSound();
-	
-	print("--------------------------------------------------------------");
+		// Because base class makeSound method has not been declared as virtual, despite a4 being a Cat, a4 will still displays WrongAnimal sound
+		a4->makeSound();
+		
+		delete a4;
 
-	print("Destructors taking place");
-	// Freeing memory from the allocated targeted
-	// (explain virtual destructor)
-	delete a1;
-	delete a2;
-	delete a3;
-	delete a4;
-
-
+	}
 	return 0;
 }
-
-// ! Bellow is the main given by the subject
-/*
-int main() {
-	const Animal* meta = new Animal();
-	const Animal* j = new Dog();
-	const Animal* i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound(); //will output the cat sound!
-	j->makeSound();
-	meta->makeSound();
-
-	// The main subject does not provide delete functions, so I did.
-	delete meta;
-	delete i;
-	delete j;
-}
-*/
