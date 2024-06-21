a7583d11c8a14b006e767f8fef460ab970036763
“florianverge” <verge.f@gmail.com>
2024-06-21 20:07:00 +0200

    😎 MAJOR UPDATE : refined tests for cpp_04_ex_01

--- cpp_04/ex01/src/main.cpp
+++ cpp_04/ex01/src/main.cpp
@@ -6,48 +6,84 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/20 13:38:55 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/21 09:16:51 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/21 20:03:14 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Animal.hpp"
 #include "Cat.hpp"
+#include "Dog.hpp"
 
 int main(void){
 
-	// Animal *a1 = new Cat("Croquette", "Miam Miam");
+	// Test for basic printing a Animal with << operator overload
+	print("----------------- TEST 1 -----------------");
+	{
+		Animal *a1 = new Cat("Croquette", "Miam Miam");
+
+		cout << *a1 << endl;
+
+		delete a1;
+	}
+
+	print("------------------ TEST 2----------------");
+	{
+		Animal *a2 = new Cat("Croquette", "Miam Miam");
 
 
-	// a1->displayThought();
+		// Actually prints the adress of a1
+		cout << a2 << endl;
 
-	// Animal a2 = *a1;
+		// Deferencing the pointer actually call the << Operator overload from Animal Class
+		cout << *a2 << endl;
+		print("----------------------------------");
+		
+		// Since a2 is a Cat, we would want to display Cat casting it something like
+		
+		// cout << (Cat)*a2 << endl;
 
-	// a2.displayThought();
+		// Unfortunatelly, this is a wrong syntax, for this we should use dynamic casting
 
-	// cout << *a2 << endl;
+		// Because a2 is a pointer, straight casting it will still display the adress.
+		
+		cout << dynamic_cast<Cat*>(a2) << endl;
+		
+		print("----------------------------------");
+		
+		// We need then to deference the whole thing with * operator first.
+		cout << *dynamic_cast<Cat*>(a2) << endl;
 
-	print("----------------------------------");
+		delete a2;
+	}
 
-	Cat c1("Miaous", "Turbo Croquette");
+	print("------------------ TEST 3----------------");
+	// Tests asked by the subject
+	{
+		Animal *animals[10];
 
-	print("Cat 1 name = "+c1.getType());
-	c1.displayThought();
+		// Init first half with Cats
+		for (size_t i = 0; i < 5; i++)
+			animals[i] = new Cat("Miaous", "Croquette 100\% poisson ");
 
-	
-	print("----------------------------------");
+		// Init second half with Dogs
+		for (size_t i = 5; i < 10; i++)
+			animals[i] = new Dog("Roberto", "Croquette 100% Boeuf");
+		
+		print("----------------------------------");
+		
+		// Printing dynamically each Cat
+		for (size_t i = 0; i < 5; i++)
+			cout << *dynamic_cast<Cat*>(animals[i]) << endl;
+		
+		// Printing dynamically each Dog
+		for (size_t i = 5; i < 10; i++)
+			cout << *dynamic_cast<Dog*>(animals[i]) << endl;
 
-	Cat c2;
-	
-	c2 = c1;
+		// Freeing each Animal
+		for (size_t i = 0; i < 10; i++)
+			delete animals[i];
+			
+	}
 
-	print("Cat 2 name = "+c2.getType());
-	
-	c2.displayThought();
-
-	
-	// delete a1;
-	// delete a2;
-
-	
 	return 0;
 }
