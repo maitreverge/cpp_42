96af5464fe47ccd0e8a030bccd86e49f8f28b72d
“maitreverge_42” <verge.f@gmail.com>
2024-06-28 10:50:28 +0200

    cpp_04_ex_03 unfinished

--- cpp_04/ex03/includes/Character.hpp
+++ cpp_04/ex03/includes/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 15:36:46 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/25 15:38:10 by flverge          ###   ########.fr       */
+/*   Updated: 2024/06/28 10:46:13 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,21 +17,18 @@
 class Character : public ICharacter
 {
 
-private:
-
-	// Some private members
-
 public:
 
 	Character( void );
+	Character( string nameInput );
 	Character( const Character& copy );
 	Character& operator=( const Character& right_operator );
 	~Character();
 
-	const string& getName( void )const = 0;
-	void equip( AMateria* m ) = 0;
-	void unequip( int idx ) = 0;
-	void use( int idx, ICharacter& target ) = 0;
+	const string& getName( void )const; // done
+	void equip( AMateria* m );
+	void unequip( int idx );
+	void use( int idx, ICharacter& target );
 
 };
 
@@ -41,18 +38,67 @@ ostream& operator<<( ostream& output_stream, const Character& input );
 // ---------- Functions declarations ---------------
 
 
-Character::Character( void ){}
+Character::Character( void ) : ICharacter(){
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		this->slots[i] = 0; // ? OR NULL 
+	}
+	
+}
+
+Character::Character( string nameInput ) : ICharacter(), _name(nameInput){};
 
 
-Character::Character( const Character& copy ){ *this = copy; }
+
+Character::Character( const Character& copy ) : ICharacter(copy){
+		
+	*this = copy;
+}
 
 
 Character::~Character( void ){}
 
 
+const string& Character::getName( void )const{ return this->_name; }
+
+
+void Character::equip( AMateria* m ){
+
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (this->slots[i] == 0){
+			this->slots[i] = m;
+			printColor(GREEN, "AMatetira " + m->getType() +
+				" successfully equiped on " + this->getName());
+			break;	
+		}
+		else if (i == 3)
+			printColor(RED, "Full Inventory, can't equip Materia " + m->getType() +
+				" on Character " + this->getName());
+	}
+	return ;
+}
+
+void Character::unequip( int idx ){
+
+	if (this->slots[idx] == 0)
+		printColor(RED, "Unequiping " + this->getName() +
+			" is impossible at given index " + customItoA(idx) + " (index already empty)");
+	else {
+
+		this->slots[idx] = 0;
+		printColor(RED, "Unequiping " + this->getName() + " successfull !");
+	}
+}
+
+
+
+
 Character& Character::operator=( const Character& right_operator ){
 
 	if (this != &right_operator){
+		f
 		// Reassign every value with the getter value 
 		// this->_foo = right_operator.getFoo()
 	}
--- cpp_04/ex03/src/ICharacter.cpp
+++ cpp_04/ex03/src/ICharacter.cpp
@@ -1,33 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ICharacter.hpp                                     :+:      :+:    :+:   */
+/*   ICharacter.cpp                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/25 15:28:02 by flverge           #+#    #+#             */
-/*   Updated: 2024/06/28 10:23:37 by flverge          ###   ########.fr       */
+/*   Created: 2024/06/28 10:06:14 by flverge           #+#    #+#             */
+/*   Updated: 2024/06/28 10:06:30 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#pragma once
+#include "ICharacter.hpp"
 
-#include "AMateria.hpp"
-
-class ICharacter
-{
-
-protected:
-
-	string _name;
-	AMateria* slots[4]; // for equiping 4 Materias maximum
-
-public:
-
-	virtual ~ICharacter( void );
-	virtual const string& getName( void )const = 0;
-	virtual void equip( AMateria* m ) = 0;
-	virtual void unequip( int idx ) = 0;
-	virtual void use( int idx, ICharacter& target ) = 0;
-	
-};
+ICharacter::~ICharacter( void ){}
