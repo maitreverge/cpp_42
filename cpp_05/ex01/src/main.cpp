542c992fa2e92a5a81668bb93eabbf3e7e02a853
“maitreverge_42” <verge.f@gmail.com>
2024-07-02 14:44:59 +0200

    refined test for cpp_05_ex_01

--- cpp_05/ex01/src/main.cpp
+++ cpp_05/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/02 09:28:10 by flverge           #+#    #+#             */
-/*   Updated: 2024/07/02 14:12:04 by flverge          ###   ########.fr       */
+/*   Updated: 2024/07/02 14:43:49 by flverge          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,59 +87,65 @@ void	test1(void){
 	print("");
 }
 
+void	test2(void){
+
+	// Declaring both a valid Bureaucrat and Form
+	Bureaucrat ulysse("Ulysse", 5);
+	Form f1("Slavery Contract", 1, 1);
+
+	print("Initial state of Ulysse");
+	cout << ulysse << endl;
+	
+	print("Initial state of Form f1");
+	cout << f1 << endl;
+
+	printColor(BOLD_GREEN, "====================== START OF THE LOOP ======================\n\n");	
+	
+	// In this test, we will try to increment Bureaucrat grade until he can sign the form
+	// We will also increment it further 1, and still try to sign it after the form being signed.
+	for (size_t i = 0; i < 7; i++)
+	{
+		print("Ulysse state before increment\n");
+		cout << ulysse << endl;
+		
+		try
+		{
+			ulysse.incrementGrade();
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		
+		print("Ulysse state after an increment try");
+		cout << ulysse << endl;
+
+		try
+		{
+			f1.beSigned(ulysse);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		print("\n--------------------------------\n");
+		
+	}
+	
+}
+
 int main(void){
 
 	string promptUser = "";
 	clearScreen();
 
-
 	triggerTest(promptUser, "TEST failed Bureaucrat and Form");
 
 	test1();
 	
+	triggerTest(promptUser, "TEST valid Bureaucrat and Form");
 
-
-	// Declaring both a valid Bureaucrat and Form
-	// Bureaucrat ulysse("Ulysse", 5);
-
-	// cout << ulysse << endl;
-
-	// Form f1("Slavery Contract", 1, 1);
+	test2();
 	
-	// cout << f1 << endl;
-
-	
-	// for (size_t i = 0; i < 7; i++)
-	// {
-	// 	print("Ulysse state before increment");
-	// 	cout << ulysse << endl << endl;
-	// 	try
-	// 	{
-	// 		ulysse.incrementGrade();
-	// 		// f1.beSigned(ulysse);
-	// 	}
-	// 	catch(const std::exception& e)
-	// 	{
-	// 		std::cerr << e.what() << '\n';
-	// 	}
-		
-	// 	print("Ulysse state after increment");
-
-	// 	try
-	// 	{
-	// 		// ulysse.incrementGrade();
-	// 		f1.beSigned(ulysse);
-	// 	}
-	// 	catch(const std::exception& e)
-	// 	{
-	// 		std::cerr << e.what() << '\n';
-	// 	}
-		
-	// }
-	
-
-	
-
-
 	return 0;
 }
