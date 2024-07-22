fc9cae21ad08f850bf0f174827ed24e7081d8ef7
“florianverge” <verge.f@gmail.com>
2024-07-22 21:16:03 +0200

    add doc to `serializer`

--- cpp_06/ex01/src/main.cpp
+++ cpp_06/ex01/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/22 20:40:33 by ubuntu            #+#    #+#             */
-/*   Updated: 2024/07/22 21:12:39 by ubuntu           ###   ########.fr       */
+/*   Updated: 2024/07/22 21:15:37 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,14 @@
 
 int main(void){
 
+    // Declaring a basic struc with 3 types of data
     Data dataMember;
 
     dataMember.x = 42;
     dataMember.y = "This is a Welcome String";
     dataMember.z = 42.42f;
     
+    // Printing out this data
     cout << "Adress of dataMember = " << BOLD_GREEN << &dataMember << RESET << endl;
     
     cout << "dataMember.x = " << CYAN << dataMember.x << RESET << endl;
@@ -28,17 +30,19 @@ int main(void){
 
     print("");
 
+    // Serializing in a unintptr_t data structure
     uintptr_t ptr = Serializer::serialize(&dataMember);
 
+    // Deserializing it within a pointer to another instance of Data struct.
     Data *newDataMember = Serializer::deserialize(ptr);
 
     print("");
     
+    // Printing out this data
     cout << "Adress of newDataMember = " BOLD_GREEN <<  &dataMember << RESET << endl;
 
     cout << "newDataMember->x = " << CYAN << newDataMember->x << RESET << endl;
     cout << "newDataMember->y = " << RED << newDataMember->y << RESET << endl;
     cout << "newDataMember->z = " << YELLOW << newDataMember->z << RESET << endl;
 
-    
 }
