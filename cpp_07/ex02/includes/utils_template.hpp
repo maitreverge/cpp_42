c4cf0dbe2ab13738f3bb262a5c96705b99f65e4a
“florianverge” <verge.f@gmail.com>
2024-07-24 10:09:39 +0200

    init files for `cpp_07`

---
+++ cpp_07/ex02/includes/utils_template.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils_template.hpp                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flverge <flverge@student.42perpignan.fr    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/05 12:24:17 by flverge           #+#    #+#             */
+/*   Updated: 2024/07/08 09:42:36 by flverge          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <ostream>
+#include <sstream>
+#include <cstdlib>
+
+
+using std::string;
+using std::cout;
+using std::endl;
+using std::cin;
+using std::fstream;
+using std::ifstream;
+using std::ofstream;
+using std::ostream;
+using std::stringstream;
+using std::exception;
+
+#include "colors.hpp" // Includes all ANSII color codes
+
+// Utils wrappers functions 
+void	print( const string& str );
+void	printNoEndl( const string& str );
+void	printColor( string COLOR, const string& str );
+void	printColorNoEndl( string COLOR, const string& str );
+void	clearScreen( void );
+void	customExit( const string& str );
+string	customItoA( const long long int& target);
+int 	customAtoI( const string& str );
+void	extraLine( void );
+bool	doesFileExists( const string& fileName);
