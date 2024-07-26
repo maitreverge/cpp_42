d2237c90493c740def4efe58d2600fc1d98549c6
“florianverge” <verge.f@gmail.com>
2024-07-26 13:39:10 +0200

    init files cpp08

---
+++ cpp_08/ex00/includes/easyfind.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/26 13:15:42 by ubuntu            #+#    #+#             */
+/*   Updated: 2024/07/26 13:19:25 by ubuntu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include "utils_template.hpp"
+
+/**
+ * @brief Find then first occurence of needle in a container of type int
+ * 
+ * @tparam T 
+ * @param haystack 
+ * @param needle 
+ * @return int 
+ */
+template < typename T >
+int easyfind( T &haystack, int needle ){
+
+    
+}
