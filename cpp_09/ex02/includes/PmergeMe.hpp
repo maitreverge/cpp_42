c9e10c39ec78e343e6768310ec069483c72e54af
“florianverge” <verge.f@gmail.com>
2024-08-18 12:03:43 +0200

    working on solutionnign the jacob sea

--- cpp_09/ex02/includes/PmergeMe.hpp
+++ cpp_09/ex02/includes/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 12:44:12 by flverge           #+#    #+#             */
-/*   Updated: 2024/08/16 11:34:43 by ubuntu           ###   ########.fr       */
+/*   Updated: 2024/08/18 12:01:33 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,50 +131,35 @@ void    PmergeMe::printTimeExecution( string input ){
 }
 
 void    PmergeMe::sortVector( void ){
-
     // Clock in
     this->_timeStartVector = std::clock();
 
-    // ! STEP 1 : if there is a odd numbers in the vector, push it away
     bool isStruggle = false;
     int struggle;
 
-    if ( _vectorContainer.size() % 2 != 0 ){
-        
+    if (_vectorContainer.size() % 2 != 0) {
         isStruggle = true;
         struggle = *(_vectorContainer.end() - 1);
         _vectorContainer.pop_back();
     }
 
-    // ! STEP 2 : make pair of two numbers from the original vector
-    std::vector< std::pair<int, int> > pairedVector;
+    // STEP 2: Make pairs of two numbers from the original vector
+    std::vector<std::pair<int, int> > pairedVector;
 
-    // Init the double vector and swap the values of big and small number is necessary
-    for ( std::vector<int>::iterator it = _vectorContainer.begin() ; it != _vectorContainer.end(); ++it)
-    {
+    // Init the double vector and swap the values of big and small number if necessary
+    for (std::vector<int>::iterator it = _vectorContainer.begin(); it != _vectorContainer.end(); ++it) {
         int bigNb = *it;
         int smallNb = *++it;
         if (bigNb < smallNb)
-            std::swap(bigNb, smallNb); 
-        pairedVector.push_back( std::make_pair(bigNb, smallNb) );
+            std::swap(bigNb, smallNb);
+        pairedVector.push_back(std::make_pair(bigNb, smallNb));
     }
 
-    
-    // printColor(BOLD_BLUE, "printing paired values vector");
-    // for ( std::vector< std::pair<int, int> >::iterator it = pairedVector.begin(); it != pairedVector.end(); ++it)
-    // {
-    //     printNoEndl("Value 1 :");
-    //     print( it->first );
-    //     printNoEndl("Value 2 :");
-    //     print(it->second);
-    // }
-    
-    // ! STEP 3 : Create two vectors of both big and small numbers
+    // STEP 3: Create two vectors of both big and small numbers
     std::vector<int> bigNumbers; // the big numbers need to be sorted
     std::vector<int> smallNumbers;
 
-    for ( std::vector< std::pair<int, int> >::iterator it = pairedVector.begin(); it != pairedVector.end(); ++it)
-    {
+    for (std::vector<std::pair<int, int> >::iterator it = pairedVector.begin(); it != pairedVector.end(); ++it) {
         int big = it->first;
         int small = it->second;
         
@@ -182,31 +167,44 @@ void    PmergeMe::sortVector( void ){
         smallNumbers.push_back(small);
     }
 
-    std::sort( bigNumbers.begin(), bigNumbers.end() );
+    std::sort(bigNumbers.begin(), bigNumbers.end());
 
-    // ! TO DO : push the first from small at the beginning to big
-
-    // print("ONE");
-    // for (std::vector<int>::iterator it = bigNumbers.begin(); it != bigNumbers.end() ; ++it)
-    // {
-    //     print(*it);
-    // }
-    
-    
+    // Insert the first element of smallNumbers into bigNumbers
     bigNumbers.insert(bigNumbers.begin(), smallNumbers.front());
     smallNumbers.erase(smallNumbers.begin());
 
-    print("Big numbers Displayed after small insertion");
-    for ( std::vector<int>::iterator it = bigNumbers.begin() ; it != bigNumbers.end() ; ++it)
-    {
+    // Calculate Jacobsthal numbers
+    std::vector<int> jacobsthalNumbers;
+    int n = smallNumbers.size();
+    for (int i = 0; i < n; ++i) {
+        int jacobsthal = (i == 0) ? 0 : (i == 1) ? 1 : jacobsthalNumbers[i - 1] + 2 * jacobsthalNumbers[i - 2];
+        jacobsthalNumbers.push_back(jacobsthal);
+    }
+
+    // Merge the remaining smallNumbers into bigNumbers using Jacobsthal numbers
+    for (size_t i = 1; i < jacobsthalNumbers.size(); ++i) {
+        size_t pos = jacobsthalNumbers[i];
+        if (pos < smallNumbers.size()) {
+            std::vector<int>::iterator insertPos = std::lower_bound(bigNumbers.begin(), bigNumbers.end(), smallNumbers[pos]);
+            bigNumbers.insert(insertPos, smallNumbers[pos]);
+        }
+    }
+
+    // If there was an odd number of elements, insert the struggle element
+    if (isStruggle) {
+        std::vector<int>::iterator pos = std::lower_bound(bigNumbers.begin(), bigNumbers.end(), struggle);
+        bigNumbers.insert(pos, struggle);
+    }
+
+    // Replace the original vector with the sorted vector
+    this->_vectorContainer = bigNumbers;
+
+    // Print the sorted bigNumbers
+    print("Sorted numbers:");
+    for (std::vector<int>::iterator it = bigNumbers.begin(); it != bigNumbers.end(); ++it) {
         print(*it);
     }
-    
-    
-    
-    (void)isStruggle;
-    (void)struggle;
-    // Clock out
+
     this->_timeEndVector = std::clock();
 }
 
