5363dacafc4326225f9fda3c0f12c7fe5bc8407c
“maitreverge_42” <verge.f@gmail.com>
2024-08-20 13:49:16 +0200

    Fixed jsonConvert prog

--- convertJSONdebugARGS.cpp
+++ convertJSONdebugARGS.cpp
@@ -3,29 +3,30 @@
 #include <string>
 
 using namespace std;
+
 int main (int ac, char **av){
 
     if ( ac != 2)
         cout << "usage incorrect" << endl;
 
-    string remplacing = "\", \"";
-
     string mainArg(av[1]);
 
-    // insert Double quote at the begining and the end
+    // String to separate eahc argument with a  ", "    string    
+    string targetString = "\", \"";
 
+    // insert Double quote at the begining and the end
     mainArg.insert(mainArg.begin(), '\"');
     mainArg.insert(mainArg.end(), '\"');
 
+    // Replacing each string with the 
     for (size_t i = 0; i < mainArg.size(); ++i)
     {
         if ( std::isspace(mainArg[i])){
 
-            mainArg.replace(i, 1, remplacing);
-            i += remplacing.size() - 1;
+            mainArg.replace(i, 1, targetString);
+            i += targetString.size() - 1;
         }
     }
-    
-    cout << mainArg << endl;
 
+    cout << mainArg << endl;
 }
